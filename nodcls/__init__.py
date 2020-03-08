71d4fa24bfb42583e8c022045e9c1e7a7e33cb17
SYSY-Momojie <1069614543@qq.com>
2020-03-09 00:45:25 +0800

    train

--- nodcls/__init__.py
+++ nodcls/__init__.py
@@ -152,7 +152,7 @@ def get_file_list(args):
             tefnamelst.append(srsid + '.npy')
             telabellst.append(int(label))
             tefeatlst.append(feat)
-        if random.randint(0, 9) != 9:
+        if srsid.split('-')[0] not in test_id_list or random.randint(0, 9) < 5:
             trfnamelst.append(srsid + '.npy')
             trlabellst.append(int(label))
             trfeatlst.append(feat)
@@ -255,7 +255,7 @@ def run_train():
         _, tr = train(net, loss, opt, train_loader, epoch, args.epochs, m)
         _, te = test(net, loss, test_loader, m)
 
-        to_save = False
+        to_save = True
         if tr > best_tr:
             best_tr = tr
             to_save = True
